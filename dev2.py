71e72b0f0142d25e7a60ca73bb29011c165f815c
Imsurajkr <infosurajkumard@gmail.com>
2020-02-15 18:25:08 +0530

    Updates

--- dev2.py
+++
@@ -1,6 +0,0 @@
-print("This file is edited bbyb developer 2")
-
-
-
-
-print("This workload is developed by suraj ")
--- new.py
+++
@@ -1,7 +0,0 @@
-print("Hey This is my code ")
-
-print("My second UPdates to the local git server ")
-
-
-
-print("Hey Hitanshu this is my file")
